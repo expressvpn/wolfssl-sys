d8df03ad20482162ae1c5a3f1c8046254e98988c
Pang T <126134360+expressvpn-pang-t@users.noreply.github.com>
2023-03-20 13:46:56 +0800

    wolfssl: Add bindings for the various `*_client_method()` APIs

--- wolfssl/src/lib.rs
+++ wolfssl/src/lib.rs
@@ -42,9 +42,52 @@ pub fn wolf_cleanup() -> Result<(), WolfCleanupError> {
     }
 }
 
+// We support these for now. The methods here correspond directly to
+// the build flags we currently set in `build.rs`
+pub enum WolfMethod {
+    DtlsClient,
+    DtlsClientV1_2,
+    DtlsClientV1_3,
+    DtlsServer,
+    DtlsServerV1_2,
+    DtlsServerV1_3,
+    TlsClient,
+    TlsClientV1_2,
+    TlsClientV1_3,
+    TlsServer,
+    TlsServerV1_2,
+    TlsServerV1_3,
+}
+
+impl WolfMethod {
+    pub fn into_method_ptr(self) -> Option<*mut raw_bindings::WOLFSSL_METHOD> {
+        let ptr = match self {
+            Self::DtlsClient => unsafe { raw_bindings::wolfDTLS_client_method() },
+            Self::DtlsClientV1_2 => unsafe { raw_bindings::wolfDTLSv1_2_client_method() },
+            Self::DtlsClientV1_3 => unsafe { raw_bindings::wolfDTLSv1_3_client_method() },
+            Self::DtlsServer => unsafe { raw_bindings::wolfDTLS_server_method() },
+            Self::DtlsServerV1_2 => unsafe { raw_bindings::wolfDTLSv1_2_server_method() },
+            Self::DtlsServerV1_3 => unsafe { raw_bindings::wolfDTLSv1_3_server_method() },
+            Self::TlsClient => unsafe { raw_bindings::wolfTLS_client_method() },
+            Self::TlsClientV1_2 => unsafe { raw_bindings::wolfTLSv1_2_client_method() },
+            Self::TlsClientV1_3 => unsafe { raw_bindings::wolfTLSv1_3_client_method() },
+            Self::TlsServer => unsafe { raw_bindings::wolfTLS_server_method() },
+            Self::TlsServerV1_2 => unsafe { raw_bindings::wolfTLSv1_2_server_method() },
+            Self::TlsServerV1_3 => unsafe { raw_bindings::wolfTLSv1_3_server_method() },
+        };
+
+        if !ptr.is_null() {
+            Some(ptr)
+        } else {
+            None
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
 
     #[test]
     fn wolf_init_test() {
@@ -53,6 +96,24 @@ mod tests {
 
     #[test]
     fn wolf_cleanup_test() {
+        wolf_cleanup().unwrap();
+    }
+
+    #[test_case(WolfMethod::DtlsClient)]
+    #[test_case(WolfMethod::DtlsClientV1_2)]
+    #[test_case(WolfMethod::DtlsClientV1_3)]
+    #[test_case(WolfMethod::DtlsServer)]
+    #[test_case(WolfMethod::DtlsServerV1_2)]
+    #[test_case(WolfMethod::DtlsServerV1_3)]
+    #[test_case(WolfMethod::TlsClient)]
+    #[test_case(WolfMethod::TlsClientV1_2)]
+    #[test_case(WolfMethod::TlsClientV1_3)]
+    #[test_case(WolfMethod::TlsServer)]
+    #[test_case(WolfMethod::TlsServerV1_2)]
+    #[test_case(WolfMethod::TlsServerV1_3)]
+    fn wolfssl_context_new(method: WolfMethod) {
+        wolf_init().unwrap();
+        let _ = method.into_method_ptr().unwrap();
         wolf_cleanup().unwrap();
     }
 }
