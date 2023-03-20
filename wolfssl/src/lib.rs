7441d3b2fd51e0cb9b1c218f0ecb23e49b30b5e9
Pang T <126134360+expressvpn-pang-t@users.noreply.github.com>
2023-03-20 13:25:29 +0800

    wolfssl: Add bindings for `wolfSSL_Init` and `wolfSSL_Cleanup`

--- wolfssl/src/lib.rs
+++ wolfssl/src/lib.rs
@@ -1,1 +1,58 @@
+use wolfssl_sys::raw_bindings;
 
+/// Return error values for [`wolf_init`]
+#[derive(Debug)]
+pub enum WolfInitError {
+    /// Corresponds with `BAD_MUTEX_E`
+    Mutex,
+    /// Corresponds with `WC_INIT_E`
+    WolfCrypt,
+}
+
+/// Wraps [`wolfSSL_Init`][0]
+///
+/// Note that this is also internally during initialization by
+/// [`WolfSslContext`].
+///
+/// [0]: https://www.wolfssl.com/documentation/manuals/wolfssl/group__TLS.html#function-wolfssl_init
+pub fn wolf_init() -> Result<(), WolfInitError> {
+    match unsafe { raw_bindings::wolfSSL_Init() } {
+        raw_bindings::WOLFSSL_SUCCESS => Ok(()),
+        raw_bindings::BAD_MUTEX_E => Err(WolfInitError::Mutex),
+        raw_bindings::WC_INIT_E => Err(WolfInitError::WolfCrypt),
+        e => panic!("Unexpected return value from `wolfSSL_Init`. Got {e}"),
+    }
+}
+
+/// Return error values for [`wolf_cleanup`]
+#[derive(Debug)]
+pub enum WolfCleanupError {
+    /// Corresponds with `BAD_MUTEX_E`
+    Mutex,
+}
+
+/// Wraps [`wolfSSL_Cleanup`][0]
+///
+/// [0]: https://www.wolfssl.com/documentation/manuals/wolfssl/group__TLS.html#function-wolfssl_cleanup
+pub fn wolf_cleanup() -> Result<(), WolfCleanupError> {
+    match unsafe { raw_bindings::wolfSSL_Cleanup() } {
+        raw_bindings::WOLFSSL_SUCCESS => Ok(()),
+        raw_bindings::BAD_MUTEX_E => Err(WolfCleanupError::Mutex),
+        e => panic!("Unexpected return value from `wolfSSL_Cleanup. Got {e}`"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn wolf_init_test() {
+        wolf_init().unwrap();
+    }
+
+    #[test]
+    fn wolf_cleanup_test() {
+        wolf_cleanup().unwrap();
+    }
+}
